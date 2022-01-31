df8663df91f367f35c3c733184981db18fa13362
Namra <Namra@192.168.1.205>
2022-01-31 17:15:39 +0530

    try

---
+++ Java_Project/src/Strings_Demo1.java
@@ -0,0 +1,19 @@
+import java.io.*;
+	import java.lang.*;
+	 
+public class Strings_Demo1 {
+
+	    public static void main(String[] args)
+	    {
+	        
+	        String s = "Arman";
+	 
+	        System.out.println("String s = " + s);
+	 
+	        
+	        String s1 = new String("Arman");
+	 
+	        System.out.println("String s1 = " + s1);
+	    }
+
+}
