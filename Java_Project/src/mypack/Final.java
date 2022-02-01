3424eb90c959f0b9d572c4e806eb1183e9734ae0
Namra <Namra@192.168.1.205>
2022-02-01 16:56:10 +0530

    final

--- Java_Project/src/mypack/Final.java
+++ Java_Project/src/mypack/Final.java
@@ -6,13 +6,9 @@ public class Final {
 	 public static void main(String args[])
 	{ 
 	 A obj = new A(); 
-	// B obj1 = new B(); 
-	// C obj2 = new C();
-	 D obj3 = new D();
+	 D obj1 = new D();
 	 obj.msg(); 
-	// obj1.msg(); 
-	// obj2.msg();
-	 obj3.msg();
+	 obj1.msg();
 	 } 
 	}
 
